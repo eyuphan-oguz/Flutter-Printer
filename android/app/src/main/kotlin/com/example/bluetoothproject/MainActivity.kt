9f6c432fd6c51a0fa0ccd571100e9abb3fe1cf45
eyuphan-oguz <75530935+eyuphan-oguz@users.noreply.github.com>
2022-08-27 14:06:36 +0300

    first

---
+++ android/app/src/main/kotlin/com/example/bluetoothproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bluetoothproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:bluetoothproject/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Thermal Printer',
+      home: Home(),
+    );
+  }
+}
